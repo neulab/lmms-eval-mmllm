aa15a41d957a66aeb7705a3c902e8f7b1d761027
Xiang Yue <yue.149@osu.edu>
2024-08-26 11:12:02 -0400

    revise cvqa

--- lmms_eval/tasks/cvqa/utils.py
+++ lmms_eval/tasks/cvqa/utils.py
@@ -1,15 +1,13 @@
 import re
-import logging
 import random
-from datasets import Image
 from difflib import SequenceMatcher
 
 def cvqa_doc_to_text(doc):
-    question, choices = doc["Question"], doc["Options"]
+    question, choices = doc["Translated Question"], doc["Translated Options"]
     len_choices = len(choices)
     options = [chr(ord("A") + i) for i in range(len_choices)]
     choices_str = "\n".join([f"{option}. {choice}" for option, choice in zip(options, choices)])
-    return f"Question: {question} Options: {choices_str} Answer with the option's letter from the given choices directly."
+    return f"Question: {question} \n\n Options: {choices_str} \n\n Answer with the option's letter from the given choices directly."
 
 
 def cvqa_doc_to_visual(doc):
@@ -60,15 +58,15 @@ def parse_multi_choice_response(response, options):
 def cvqa_process_results(doc, results):
     # I know this is weird, but it's how llava parse it.
     target = cvqa_doc_to_target(doc)
-    pred = parse_multi_choice_response(results[0],doc['Options'])
+    pred = parse_multi_choice_response(results[0],doc['Translated Options'])
     pred_numerical = {'A':0, 'B':1, 'C':2, 'D':3}[pred]
 
-    with open ("cvqa_predictions.csv", "a") as f:
+    with open ("cvqa_submissions/llava1.5-cvqa_predictions_en.csv", "a") as f:
         f.write(f"{doc['ID']},{pred_numerical}\n")
     if pred == target:
         return {"exact_match": 1.0}
     # pattern: ^[A-Z]\. .*
-    if len(pred) >= 2 and pred[0].isupper() and pred[1] == ".":
+    if len(pred) >= 2 and pred[0].isupper() and pred[1] == ".": 
         result = 1.0 if pred[0] == target else 0.0
         return {"exact_match": result}
     return {"exact_match": 0.0}
