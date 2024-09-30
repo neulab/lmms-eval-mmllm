51db34c293adedcde11cb83b1272ed9ef942e639
SeungoneKim <seungone@cmu.edu>
2024-09-30 00:00:56 -0400

    update prompts

--- lmms_eval/tasks/xchat/response_gen.py
+++ lmms_eval/tasks/xchat/response_gen.py
@@ -6,7 +6,7 @@ from PIL import Image
 import requests
 from tqdm import tqdm
 import torch
-from transformers import AutoProcessor, LlavaForConditionalGeneration, LlavaNextForConditionalGeneration
+from transformers import AutoProcessor, LlavaForConditionalGeneration, LlavaNextForConditionalGeneration, PaliGemmaForConditionalGeneration, Blip2ForConditionalGeneration, Blip2Processor, AutoModelForCausalLM
 
 
 def load_and_process_image(image_path):
@@ -48,7 +48,7 @@ def main(args):
     #     processor = AutoProcessor.from_pretrained(args.model_name)
     # else:
     #     processor = AutoProcessor.from_pretrained("llava-hf/llava-1.5-7b-hf")
-    processor = AutoProcessor.from_pretrained(args.model_name)
+    
     # processor.tokenizer.add_tokens(["<image>", "<pad>"], special_tokens=True)
 
     prompts, images, orig_data = prep_dataset(args.language)
@@ -60,22 +60,65 @@ def main(args):
             cache_dir="/data/tir/projects/tir7/user_data/seungonk/huggingface"
         ).to(0)
         model.resize_token_embeddings(len(processor.tokenizer))
-    else:
+        processor = AutoProcessor.from_pretrained(args.model_name)
+    elif "3.5" in args.model_name:
+        model = AutoModelForCausalLM.from_pretrained("microsoft/Phi-3.5-vision-instruct", torch_dtype=torch.bfloat16, trust_remote_code=True,cache_dir="/data/tir/projects/tir7/user_data/seungonk/huggingface").to(0)
+        processor = AutoProcessor.from_pretrained(args.model_name,trust_remote_code=True,num_crops=4)
+    elif "mix-448" in args.model_name:
+        model = PaliGemmaForConditionalGeneration.from_pretrained("google/paligemma-3b-mix-448",trust_remote_code=True,torch_dtype=torch.bfloat16,cache_dir="/data/tir/projects/tir7/user_data/seungonk/huggingface").to(0)
+        processor = AutoProcessor.from_pretrained(args.model_name,trust_remote_code=True)
+    elif "1.6" in args.model_name:
         model = LlavaNextForConditionalGeneration.from_pretrained(
             args.model_name, 
             torch_dtype=torch.float16,
             cache_dir="/data/tir/projects/tir7/user_data/seungonk/huggingface"
         ).to(0)
         model.resize_token_embeddings(len(processor.tokenizer))
+        processor = AutoProcessor.from_pretrained(args.model_name)
+    elif "mblip-mt0-xl" in args.model_name:
+        processor = Blip2Processor.from_pretrained("Gregor/mblip-mt0-xl")
+        model = Blip2ForConditionalGeneration.from_pretrained("Gregor/mblip-mt0-xl", torch_dtype=torch.bfloat16, cache_dir="/data/tir/projects/tir7/user_data/seungonk/huggingface").to(0)
+    elif "mblip-bloomz-7b" in args.model_name:
+        processor = Blip2Processor.from_pretrained("Gregor/mblip-bloomz-7b")
+        model = Blip2ForConditionalGeneration.from_pretrained("Gregor/mblip-bloomz-7b", torch_dtype=torch.bfloat16, cache_dir="/data/tir/projects/tir7/user_data/seungonk/huggingface").to(0)
+    else:
+        raise ValueError(f"Model name {args.model_name} is not supported")
 
     results = []
     for p,i in tqdm(zip(prompts,images),total=len(images)):
-        p = processor.apply_chat_template([p], add_generation_prompt=True)
+        if "1.5" in args.model_name or "1.6" in args.model_name:
+            p = processor.apply_chat_template([p], add_generation_prompt=True)
+            model_inputs = processor(images=i, text=p, return_tensors='pt').to("cuda", torch.float16)
+            output = model.generate(**model_inputs, max_new_tokens=1024, min_new_tokens=32, temperature=1.0, top_p=0.9, do_sample=True)
+            output = output[0]
+            result = processor.decode(output, skip_special_tokens=True, clean_up_tokenization_spaces=False)
+        elif "3.5" in args.model_name:
+            p = f"<|user|>\n<|image_1|>\n{p['content'][1]['text']}<|end|>\n<|assistant|>\n"
+            model_inputs = processor(images=i, text=p, return_tensors='pt').to("cuda", torch.bfloat16)
+            output = model.generate(**model_inputs, max_new_tokens=1024, temperature=1.0, top_p=0.9, do_sample=True, eos_token_id=processor.tokenizer.eos_token_id)
+            output = output[:, model_inputs['input_ids'].shape[1]:]
+            result = processor.batch_decode(output, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+        elif "mblip" in args.model_name:
+            p = "Question: "+ p['content'][1]['text'].split("\n\n")[1] + f"\nAnswer in {args.language}: "
+            model_inputs = processor(images=i, text=p, return_tensors='pt').to("cuda", torch.bfloat16)
+            output = model.generate(**model_inputs, max_new_tokens=1024, min_new_tokens=32, repetition_penalty=1.1, top_p=0.9, do_sample=True)
+            output = output[0]
+            result = processor.decode(output, skip_special_tokens=True, clean_up_tokenization_spaces=False)
+        elif "paligemma" in args.model_name:
+            p = "Question: "+ p['content'][1]['text'].split("\n\n")[1] + f"\nAnswer in {args.language}: "
+            model_inputs = processor(images=i, text=p, return_tensors='pt').to("cuda", torch.bfloat16)
+            output = model.generate(**model_inputs, max_new_tokens=1024, repetition_penalty=1.1, top_p=0.9, temperature=1.0, do_sample=True)
+            output = output[0]
+            result = processor.decode(output, skip_special_tokens=True, clean_up_tokenization_spaces=False)
         
-        model_inputs = processor(images=i, text=p, return_tensors='pt').to(0, torch.float16)
+        if "Answer in" in result:
+            result = result.split(f"Answer in {args.language}")[1]
+        elif "ASSISTANT:" in result:
+            result = result.split("ASSISTANT:")[1]
+        elif "<|assistant|>" in result:
+            result = result.split("<|assistant|>")[1]
         
-        output = model.generate(**model_inputs, max_new_tokens=1024, temperature=1.0, top_p=0.9, do_sample=True)
-        results.append(processor.decode(output[0][2:], skip_special_tokens=True))
+        results.append(result)
 
     for inst,r in zip(orig_data,results):
         inst['response'] = r
--- lmms_eval/tasks/xchat/response_gen.sh
+++ lmms_eval/tasks/xchat/response_gen.sh
@@ -23,31 +23,63 @@ export HF_CACHE="/data/tir/projects/tir7/user_data/seungonk/huggingface"
 # English
 # python3 response_gen.py --model_name "llava-hf/llava-1.5-7b-hf" --language "English" --output_file "./responses/llava-1.5-7b-hf-English.json" --gpu_num 1
 # python3 response_gen.py --model_name "llava-hf/llava-v1.6-vicuna-7b-hf" --language "English" --output_file "./responses/llava-v1.6-vicuna-7b-hf-English.json" --gpu_num 1
+python3 response_gen.py --model_name "Gregor/mblip-mt0-xl" --language "English" --output_file "./responses/mblip-mt0-xl-English.json" --gpu_num 1
+python3 response_gen.py --model_name "Gregor/mblip-bloomz-7b" --language "English" --output_file "./responses/mblip-bloomz-7b-English.json" --gpu_num 1
+python3 response_gen.py --model_name "google/paligemma-3b-mix-448" --language "English" --output_file "./responses/paligemma-3b-mix-448-English.json" --gpu_num 1
+python3 response_gen.py --model_name "microsoft/Phi-3.5-vision-instruct" --language "English" --output_file "./responses/Phi-3.5-vision-instruct-English.json" --gpu_num 1
 
 # Chinese
 # python3 response_gen.py --model_name "llava-hf/llava-1.5-7b-hf" --language "Chinese" --output_file "./responses/llava-1.5-7b-hf-Chinese.json" --gpu_num 1
 # python3 response_gen.py --model_name "llava-hf/llava-v1.6-vicuna-7b-hf" --language "Chinese" --output_file "./responses/llava-v1.6-vicuna-7b-hf-Chinese.json" --gpu_num 1
+python3 response_gen.py --model_name "Gregor/mblip-mt0-xl" --language "Chinese" --output_file "./responses/mblip-mt0-xl-Chinese.json" --gpu_num 1
+python3 response_gen.py --model_name "Gregor/mblip-bloomz-7b" --language "Chinese" --output_file "./responses/mblip-bloomz-7b-Chinese.json" --gpu_num 1
+python3 response_gen.py --model_name "google/paligemma-3b-mix-448" --language "Chinese" --output_file "./responses/paligemma-3b-mix-448-Chinese.json" --gpu_num 1
+python3 response_gen.py --model_name "microsoft/Phi-3.5-vision-instruct" --language "Chinese" --output_file "./responses/Phi-3.5-vision-instruct-Chinese.json" --gpu_num 1
 
 # Hindi
 # python3 response_gen.py --model_name "llava-hf/llava-1.5-7b-hf" --language "Hindi" --output_file "./responses/llava-1.5-7b-hf-Hindi.json" --gpu_num 1
 # python3 response_gen.py --model_name "llava-hf/llava-v1.6-vicuna-7b-hf" --language "Hindi" --output_file "./responses/llava-v1.6-vicuna-7b-hf-Hindi.json" --gpu_num 1
+python3 response_gen.py --model_name "Gregor/mblip-mt0-xl" --language "Hindi" --output_file "./responses/mblip-mt0-xl-Hindi.json" --gpu_num 1
+python3 response_gen.py --model_name "Gregor/mblip-bloomz-7b" --language "Hindi" --output_file "./responses/mblip-bloomz-7b-Hindi.json" --gpu_num 1
+python3 response_gen.py --model_name "google/paligemma-3b-mix-448" --language "Hindi" --output_file "./responses/paligemma-3b-mix-448-Hindi.json" --gpu_num 1
+python3 response_gen.py --model_name "microsoft/Phi-3.5-vision-instruct" --language "Hindi" --output_file "./responses/Phi-3.5-vision-instruct-Hindi.json" --gpu_num 1
 
 # Indonesian
 # python3 response_gen.py --model_name "llava-hf/llava-1.5-7b-hf" --language "Indonesian" --output_file "./responses/llava-1.5-7b-hf-Indonesian.json" --gpu_num 1
-python3 response_gen.py --model_name "llava-hf/llava-v1.6-vicuna-7b-hf" --language "Indonesian" --output_file "./responses/llava-v1.6-vicuna-7b-hf-Indonesian.json" --gpu_num 1
+# python3 response_gen.py --model_name "llava-hf/llava-v1.6-vicuna-7b-hf" --language "Indonesian" --output_file "./responses/llava-v1.6-vicuna-7b-hf-Indonesian.json" --gpu_num 1
+python3 response_gen.py --model_name "Gregor/mblip-mt0-xl" --language "Indonesian" --output_file "./responses/mblip-mt0-xl-Indonesian.json" --gpu_num 1
+python3 response_gen.py --model_name "Gregor/mblip-bloomz-7b" --language "Indonesian" --output_file "./responses/mblip-bloomz-7b-Indonesian.json" --gpu_num 1
+python3 response_gen.py --model_name "google/paligemma-3b-mix-448" --language "Indonesian" --output_file "./responses/paligemma-3b-mix-448-Indonesian.json" --gpu_num 1
+python3 response_gen.py --model_name "microsoft/Phi-3.5-vision-instruct" --language "Indonesian" --output_file "./responses/Phi-3.5-vision-instruct-Indonesian.json" --gpu_num 1
 
 # Japanese
 # python3 response_gen.py --model_name "llava-hf/llava-1.5-7b-hf" --language "Japanese" --output_file "./responses/llava-1.5-7b-hf-Japanese.json" --gpu_num 1
-python3 response_gen.py --model_name "llava-hf/llava-v1.6-vicuna-7b-hf" --language "Japanese" --output_file "./responses/llava-v1.6-vicuna-7b-hf-Japanese.json" --gpu_num 1
+# python3 response_gen.py --model_name "llava-hf/llava-v1.6-vicuna-7b-hf" --language "Japanese" --output_file "./responses/llava-v1.6-vicuna-7b-hf-Japanese.json" --gpu_num 1
+python3 response_gen.py --model_name "Gregor/mblip-mt0-xl" --language "Japanese" --output_file "./responses/mblip-mt0-xl-Japanese.json" --gpu_num 1
+python3 response_gen.py --model_name "Gregor/mblip-bloomz-7b" --language "Japanese" --output_file "./responses/mblip-bloomz-7b-Japanese.json" --gpu_num 1
+python3 response_gen.py --model_name "google/paligemma-3b-mix-448" --language "Japanese" --output_file "./responses/paligemma-3b-mix-448-Japanese.json" --gpu_num 1
+python3 response_gen.py --model_name "microsoft/Phi-3.5-vision-instruct" --language "Japanese" --output_file "./responses/Phi-3.5-vision-instruct-Japanese.json" --gpu_num 1
 
 # Kinyarwanda
 # python3 response_gen.py --model_name "llava-hf/llava-1.5-7b-hf" --language "Kinyarwanda" --output_file "./responses/llava-1.5-7b-hf-Kinyarwanda.json" --gpu_num 1
-python3 response_gen.py --model_name "llava-hf/llava-v1.6-vicuna-7b-hf" --language "Kinyarwanda" --output_file "./responses/llava-v1.6-vicuna-7b-hf-Kinyarwanda.json" --gpu_num 1
+# python3 response_gen.py --model_name "llava-hf/llava-v1.6-vicuna-7b-hf" --language "Kinyarwanda" --output_file "./responses/llava-v1.6-vicuna-7b-hf-Kinyarwanda.json" --gpu_num 1
+python3 response_gen.py --model_name "Gregor/mblip-mt0-xl" --language "Kinyarwanda" --output_file "./responses/mblip-mt0-xl-Kinyarwanda.json" --gpu_num 1
+python3 response_gen.py --model_name "Gregor/mblip-bloomz-7b" --language "Kinyarwanda" --output_file "./responses/mblip-bloomz-7b-Kinyarwanda.json" --gpu_num 1
+python3 response_gen.py --model_name "google/paligemma-3b-mix-448" --language "Kinyarwanda" --output_file "./responses/paligemma-3b-mix-448-Kinyarwanda.json" --gpu_num 1
+python3 response_gen.py --model_name "microsoft/Phi-3.5-vision-instruct" --language "Kinyarwanda" --output_file "./responses/Phi-3.5-vision-instruct-Kinyarwanda.json" --gpu_num 1
 
 # Korean
 # python3 response_gen.py --model_name "llava-hf/llava-1.5-7b-hf" --language "Korean" --output_file "./responses/llava-1.5-7b-hf-Korean.json" --gpu_num 1
-python3 response_gen.py --model_name "llava-hf/llava-v1.6-vicuna-7b-hf" --language "Korean" --output_file "./responses/llava-v1.6-vicuna-7b-hf-Korean.json" --gpu_num 1
+# python3 response_gen.py --model_name "llava-hf/llava-v1.6-vicuna-7b-hf" --language "Korean" --output_file "./responses/llava-v1.6-vicuna-7b-hf-Korean.json" --gpu_num 1
+python3 response_gen.py --model_name "Gregor/mblip-mt0-xl" --language "Korean" --output_file "./responses/mblip-mt0-xl-Korean.json" --gpu_num 1
+python3 response_gen.py --model_name "Gregor/mblip-bloomz-7b" --language "Korean" --output_file "./responses/mblip-bloomz-7b-Korean.json" --gpu_num 1
+python3 response_gen.py --model_name "google/paligemma-3b-mix-448" --language "Korean" --output_file "./responses/paligemma-3b-mix-448-Korean.json" --gpu_num 1
+python3 response_gen.py --model_name "microsoft/Phi-3.5-vision-instruct" --language "Korean" --output_file "./responses/Phi-3.5-vision-instruct-Korean.json" --gpu_num 1
 
 # Spanish
 # python3 response_gen.py --model_name "llava-hf/llava-1.5-7b-hf" --language "Spanish" --output_file "./responses/llava-1.5-7b-hf-Spanish.json" --gpu_num 1
-python3 response_gen.py --model_name "llava-hf/llava-v1.6-vicuna-7b-hf" --language "Spanish" --output_file "./responses/llava-v1.6-vicuna-7b-hf-Spanish.json" --gpu_num 1
+# python3 response_gen.py --model_name "llava-hf/llava-v1.6-vicuna-7b-hf" --language "Spanish" --output_file "./responses/llava-v1.6-vicuna-7b-hf-Spanish.json" --gpu_num 1
+python3 response_gen.py --model_name "Gregor/mblip-mt0-xl" --language "Spanish" --output_file "./responses/mblip-mt0-xl-Spanish.json" --gpu_num 1
+python3 response_gen.py --model_name "Gregor/mblip-bloomz-7b" --language "Spanish" --output_file "./responses/mblip-bloomz-7b-Spanish.json" --gpu_num 1
+python3 response_gen.py --model_name "google/paligemma-3b-mix-448" --language "Spanish" --output_file "./responses/paligemma-3b-mix-448-Spanish.json" --gpu_num 1
+python3 response_gen.py --model_name "microsoft/Phi-3.5-vision-instruct" --language "Spanish" --output_file "./responses/Phi-3.5-vision-instruct-Spanish.json" --gpu_num 1
